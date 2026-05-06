8f0f11e72c6c955ff2314a88eda6a34ba8f2e568
Tobib-11 <tobias.beran@student.htldornbirn.at>
2026-05-06 14:48:20 +0200

    stunde fertig

--- script/myscript.js
+++ script/myscript.js
@@ -28,10 +28,16 @@ let newPictures = [
 
 function changePics() {
   const pics = document.querySelectorAll("img");
+  const clearPlace = document.getElementsByClassName('placeclear');
   for (let i = 0; i < pics.length; i++) {
     pics[i].src = "./bilder/"+newPictures[i];
-
+  }
+  for (let x = 0; x < pics.length; x++){
+    clearPlace[x] = figures[x];
   }
 }
+
+//clear place geht nicht weil es nur zwei divs sind und zu viele namen als nächstens einfach
+//  viellecht die images holen
 
 
