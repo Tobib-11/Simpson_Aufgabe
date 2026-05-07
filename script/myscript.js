ba870cf2191770001a35ddb90e0c1482a1b10234
Tobib-11 <tobias.beran@student.htldornbirn.at>
2026-05-07 14:51:06 +0200

    exercise 4

--- script/myscript.js
+++ script/myscript.js
@@ -1,37 +1,37 @@
 // pics got changed from homer to normal
 function changePics() {
+  //names of figures
+  let figures = [
+    "apu",
+    "barney",
+    "bart",
+    "charles",
+    "glancy",
+    "grandpa",
+    "hibbert",
+    "homer",
+    "krusty",
+    "marge",
+  ];
+  //pictures
+  let newPictures = [
+    "apu_1.png",
+    "barney_2.png",
+    "bart_3.png",
+    "charles_4.png",
+    "glancy_5.png",
+    "grandpa_7.png",
+    "hibbert_6.png",
+    "homer_8.png",
+    "krusty_9.png",
+    "marge_10.png",
+    "maggie_11.png",
+    "lisa_12.png",
+  ];
   const pics = document.querySelectorAll("img");
   for (let i = 0; i < pics.length; i++) {
     pics[i].src = "./bilder/" + newPictures[i];
   }
-//names of figures
-  let figures = [
-  "apu",
-  "barney",
-  "bart",
-  "charles",
-  "glancy",
-  "grandpa",
-  "hibbert",
-  "homer",
-  "krusty",
-  "marge",
-];
-//pictures 
-let newPictures = [
-  "apu_1.png",
-  "barney_2.png",
-  "bart_3.png",
-  "charles_4.png",
-  "glancy_5.png",
-  "grandpa_7.png",
-  "hibbert_6.png",
-  "homer_8.png",
-  "krusty_9.png",
-  "marge_10.png",
-  "maggie_11.png",
-  "lisa_12.png",
-];
 
   // names bottom
   let namesOld = [
@@ -50,7 +50,8 @@ let newPictures = [
   for (let x = 0; x < namesOld.length; x++) {
     namesOld[x].innerText = figures[x];
   }
-}
 
-//clear place geht nicht weil es nur zwei divs sind und zu viele namen als nächstens einfach
-//  viellecht die images holen
+  //change color of background
+  document.getElementById("rowone").style.backgroundColor = "orange";
+  document.getElementById("rowtwo").style.backgroundColor = "blue";
+}
